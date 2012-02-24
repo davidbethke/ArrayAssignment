b38107f3a189473b8878cd0b3205af1d73c3e92e
Dave Bethke <david.bethke@gmail.com>
2012-02-24 17:15:46 -0600

    more jenkins stuff

--- Grid.cpp
+++ Grid.cpp
@@ -2,20 +2,22 @@
 //
 #include "stdafx.h"
 #include <iostream>
-
+#ifdef _DEBUG
 #include "gtest/gtest.h"
 
 TEST(sample_test_case, sample_test)
 {
 	EXPECT_EQ(1, 1);
 }
-
+#endif //_DEBUG
 int main(int argc, char** argv) 
 { 
+#ifdef _DEBUG
 	::testing::GTEST_FLAG(print_time) = true;
 	::testing::GTEST_FLAG(output) = "xml:C:\\Program Files\\Jenkins\\jobs\\GridTest\\workspace\\test_detail.xml";
-		
+	
 	testing::InitGoogleTest(&argc, argv); 
 	RUN_ALL_TESTS(); 
 	std::getchar(); // keep console window open until Return keystroke
+#endif // _DEBUG
 }
--- jenkins.bat
+++ jenkins.bat
@@ -1,2 +1,2 @@
-cd C:\Program Files\Jenkins\jobs\GridTest\workspace;
-del C:\Program Files\Jenkins\jobs\GridTest\workspace\test_detail.xml ;
+
+del .\test_detail.xml ;
